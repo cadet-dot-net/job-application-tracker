cdd3e109a24c42366da19612ea4ce7c52d31468e
cadet-dot-net <163072113+cadet-dot-net@users.noreply.github.com>
2025-06-15 13:37:58 +0100

    add basic data model as placeholder - add elm/time package

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,33 @@
 module Main exposing (main)
 
+import Browser
 import Html exposing (text)
+import Time
+
+-- MODEL
+
+type alias Application =
+  { id: Int
+  , employer: String
+  , role: String
+  , salary: Int
+  , location: List String
+  , application_date: Time.Posix
+  }
+
+type alias Model = 
+  { applications: List Application
+  , nextId: Int
+  , inputEmplyoer: String
+  , inputRole: String
+  , inputSalary: Int
+  , inputLocation: List String
+  }
 
 main =
-  text "Hello!"
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
