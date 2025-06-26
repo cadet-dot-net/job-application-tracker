0a5817c70e9af376c4369288084247ccb9b980b0
cadet-dot-net <163072113+cadet-dot-net@users.noreply.github.com>
2025-06-26 21:49:37 +0100

    declare initial board - declare default application - set month from time core package - add elm/time dependency

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (main)
 
 import Date exposing (Date)
+import Time exposing (Month(..))
 
 -- MODEL
 type alias Application =
@@ -16,6 +17,23 @@ type alias Board =
   { applications : List Application
   , nextId : Int
   , input : Application
+  }
+
+defaultApplication : Application
+defaultApplication = 
+  { id = -1
+  , employer = ""
+  , role = ""
+  , salary = 0
+  , location = []
+  , date = Date.fromCalendarDate 1999 Dec 31
+  }
+
+initBoard : Board
+initBoard =
+  { applications = []
+  , nextId = 1
+  , input = defaultApplication
   }
 
 main =
